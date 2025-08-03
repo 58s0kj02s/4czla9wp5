689ad7e5e051cab6c0b1052df298293561a7c016
Hadzz <jihathabiburrahman@gmail.com>
2025-08-03 09:56:31 +0700

    Add files via upload

---
+++ me.js
@@ -0,0 +1,316 @@
+function pw() {
+      document.body.innerHTML = ``;
+      
+      document.body.innerHTML = `
+      <div class="header">
+    <button class="back-btn" onclick="window.location.reload()" >Back</button>
+    <input type="text" id="search" placeholder="Search" onkeyup="filterCards()"/>
+  </div>
+<br><br>
+  <div class="container" id="addon-list">
+    <div class="card">
+      <h3>Mob Action Figure</h3>
+      <img src="image_1(11).jpg" alt="Mob Action Figure"/><a href="https://sfl.gl/0Lem1v" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Adidas Adventure</h3>
+      <img src="image_1(7).jpg" alt="Adidas Adventure"/><a href="https://www.mediafire.com/file/qodyudgmlutxm64/adidas_Adventurers_Add-On_%2528addon%2529_by_Hadzz.mcaddon/file" class="download-btn" target="_blank">Download Add-On</a>
+      </div>
+    <div class="card">
+      <h3>Backpacks++</h3>
+      <img src="image_1(2).jpg" alt="Backpacks++"/><a href="https://sfl.gl/hymIIq" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Action & Stuff 1.1</h3>
+      <img src="image_1(1).jpg" alt="Action & Stuff 1.1" alt="Action Stuff"/><a href="https://sfile.mobi/94Vuc7iUV24" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Action & Stuff 1.2</h3>
+      <img src="image_1(1).jpg" alt="Action & Stuff 1.2" alt="Action Stuff"/><a href="https://sfile.mobi/80rrRjABYk0" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Action & Stuff 1.3</h3>
+      <img src="image_1(1).jpg" alt="Action & Stuff 1.3" alt="Action Stuff"/><a href="https://sfile.mobi/4ydpWHaRa0b" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Action & Stuff 1.4</h3>
+      <img src="image_1(1).jpg" alt="Action & Stuff 1.4" alt="Action Stuff"/><a href="https://sfile.mobi/aZ7tuz7A1qf" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Action & Stuff 1.5</h3>
+      <img src="image_1(1).jpg" alt="Action & Stuff 1.5" alt="Action Stuff"/><a href="https://sfile.mobi/2wDcY9QVgEM" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+   <div class="card">
+      <h3>Damege Effects</h3>
+      <img src="image_1(12).jpg" alt="Damage Effects"/><a href="https://sfl.gl/OdqNGxJu" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Monster Truck</h3>
+      <img src="image_1(3).jpg" alt="Monster Truck"/><a href="https://sfl.gl/OdqNGxJu" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Aquaculture</h3>
+      <img src="image_1(13).jpg" alt="Aquaculture"/><a href="https://sfl.gl/APIhc" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Action Heroes</h3>
+      <img src="image_1.jpg" alt="Action Heroes"/><a href="https://sfile.mobi/4jsr7lyxPiJ" class="download-btn" target="_blank">Download Skin</a>
+    </div>
+    <div class="card">
+      <h3>Doors</h3>
+      <img src="image_1(10).jpg" alt="Doors"/><a href="https://www.mediafire.com/file/ibt7hg1b2gvc2y6/Doors_%2528addon%2529_by_Hadzz.mcaddon/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Portal Chests</h3>
+      <img src="image_1(4).jpg" alt="Portal Chests"/><a href="https://www.mediafire.com/file/quare26vgzbjqhu/Portal_Chests_%2528addon%2529_by_Hadzz.mcaddon/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Horror Weapons</h3>
+      <img src="image_1(6).jpg" alt="Horror Weapons"/><a href="https://sfl.gl/LmLUfA" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Food Expansion</h3>
+      <img src="image_1(9).jpg" alt="Food Expansion"/><a href="https://www.mediafire.com/file/a9vt1nmrktm788b/Food_Expansion_From_Hadzz.zip/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Wings</h3>
+      <img src="image_1(5).jpg" alt="Wings"/><a href="https://sfl.gl/MBUC" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Sky Island</h3>
+      <img src="image_1(8).jpg" alt="Sky island"/><a href="https://www.mediafire.com/file/uepw05fbw1ttris/Sky_Islands_Add-On_By_Hadzz.mcaddon/file" class="download-btn" target="_blank">Download Map</a>
+    </div>
+    <div class="card">
+      <h3>Astronaut and Aliens</h3>
+      <img src="image_1(16).jpg" alt="AAS"/><a href="https://www.mediafire.com/file/9op3zcgk8q528qq/Astronauts_and_Aliens_%2528HD%2529_%2528skin_pack%2529_by_Hadzz.mcpack/file" class="download-btn" target="_blank">Download Skin</a>
+    </div>
+    <div class="card">
+      <h3>Weapons Expansions</h3>
+      <img src="image_1(17).jpg" alt="WE"/><a href="https://www.mediafire.com/file/rilkryg4m88c0rk/Weapons_Expansion_%2528world_template%2529_by_Hadzz.mctemplate/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Minecraft Smurf</h3>
+      <img src="image_1(14).jpg" alt="MU"/><a href="https://www.mediafire.com/file/lnpk8h5sm5hvikx/Smurfs+(world_template)+by+Hadzz.mctemplate/file" class="download-btn" target="_blank">Download World</a><br>
+      <a href="https://sfile.mobi/37AtwSG5mUD" class="download-btn" target="_blank">Download Skin</a>
+    </div>
+    <div class="card">
+      <h3>Hardcore Wilderness Survival</h3>
+      <img src="image_1(15).jpg" alt="HWS"/><a href="https://www.mediafire.com/file/ko2tkq3tsumtf2r/Hardcore+Wilderness+Survival+(world_template)+by+Hadzz.mctemplate/file" class="download-btn" target="_blank">Download World</a>
+    </div>
+    <div class="card">
+      <h3>Auto Factory</h3>
+      <img src="image_1(18).jpg" alt="AF"/><a href="https://www.mediafire.com/file/00lndzqw5fasdea/Auto_Factory_RP_%2528addon%2529_by_Hadzz.mcaddon/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Armor Evolve</h3>
+      <img src="image_1(19).jpg" alt="AF"/><a href="https://www.mediafire.com/file/7amvshiqln77jzw/Armor_Evolve_Add-On_%2528addon%2529_by_Hadzz.mcaddon/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Difficulty Brutal</h3>
+      <img src="image_1(20).jpg" alt="AF"/><a href="https://www.mediafire.com/file/k9kh2a0zhr9eqbh/Difficulty_Brutal_From_Hadzz.mcaddon/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Dragon PVP</h3>
+      <img src="image_1(21).jpg" alt="AF"/><a href="https://www.mediafire.com/file/vl7qvhdb58w0kig/%25C2%25A76Dragon_PvP_%2528resources%2529_by_Hadzz.mcpack/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Everest Ascension</h3>
+      <img src="image_1(22).jpg" alt="AF"/><a href="https://www.mediafire.com/file/aa67weebmeloz0c/Everest_Ascension_%2528skin_pack%2529_by_Hadzz.mcpack/file" class="download-btn" target="_blank">Download Skin</a><br><a href="https://www.mediafire.com/file/r40srxeuiwbmqjd/Everest_Ascension_%2528world_template%2529_by_Hadzz.mctemplate/file" class="download-btn" target="_blank">Download World</a>
+    </div>
+    <div class="card">
+      <h3>Furniture++</h3>
+      <img src="image_1(23).jpg" alt="AF"/><a href="https://www.mediafire.com/file/3iupm675odv4mf4/FURNITURE%252B%252B_Add-On_%2528addon%2529_by_Hadzz.mcaddon/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Magic</h3>
+      <img src="image_1(24).jpg" alt="AF"/><a href="https://www.mediafire.com/file/55yshveqpy7gqjn/Magic_Add-On_%2528addon%2529_by_Hadzz.mcaddon/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Ambatukam Creeper</h3>
+      <img src="image_1(25).jpg" alt="AF"/><a href="https://www.mediafire.com/file/h713knenjm2485i/ambatukam_creeper_by_Hadzz.mcpack.zip/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+    <div class="card">
+      <h3>Villager Bicara</h3>
+      <img src="image_1(26).jpg" alt="AF"/><a href="https://www.mediafire.com/file/wqehywakwjtk7x2/Villager_Bicara.zip/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+        <div class="card">
+      <h3>Trains</h3>
+      <img src="image_1(27).jpg" alt="AF"/><a href="https://www.mediafire.com/file/l0213u6f9yrtrz9/Trains_Add-On_%2528addon%2529_By_Hadzz.mcaddon/file" class="download-btn" target="_blank">Download Add-On</a>
+    </div>
+  </div>
+  <style>
+  body {
+  margin: 0;
+  font-family: sans-serif;
+  background: white;
+  align-items: center;
+}
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 15px;
+}
+
+.back-btn {
+  background: #2980b9;
+  color: white;
+  border: 2px solid white;
+  border-radius: 25px;
+  padding: 10px 35px;
+  margin-bottom: 10px;
+  cursor: pointer;
+  font-size: 16px;
+  margin-top: 45px;
+  margin-right: 212px;
+  font-weight: 500;
+  position: relative;
+  z-index: 30;
+  box-shadow: 0 4px 6px rgba(0,0,0,0,3);
+  position: fixed;
+}
+
+#search {
+  padding: 10px;
+  width: 80%;
+  max-width: 400px;
+  border-radius: 15px;
+  border: 2px solid black;
+  font-size: 16px;
+  position: fixed;
+  z-index: 30;
+}
+.container {
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 40px;
+}
+
+.card {
+  background: #2980b9;
+  border-radius: 15px;
+  padding: 10px;
+  margin: 10px 0;
+  width: 90%;
+  max-width: 500px;
+  text-align: center;
+  color: white;
+  box-shadow: 0 4px 5px rgba(0,0,0,0.3);
+  border: 2px solid rainbow;
+}
+.card img {
+  width: 90%;
+  border-radius: 10px;
+  margin-top: 10px;
+  animation: zoom 2s infinite;
+}
+@keyframes zoom {
+  0% { transform: scale(1); }
+  50% { transform: scale(1.04) }
+  100% { transform: scale(1) }
+}
+.download-btn {
+  display: inline-block;
+  margin-top: 10px;
+  background: #ffdb24;
+  color: #000;
+  padding: 8px 16px;
+  border-radius: 20px;
+  text-decoration: none;
+  font-weight: bold;
+  transition: background 0.2s ease;
+}
+
+.download-btn:hover {
+  background: #d4ac0d;
+}
+</style>
+  `;
+  }
+  function filterCards() {
+  const input = document.getElementById('search').value.toLowerCase();
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    const title = card.querySelector('h3').textContent.toLowerCase();
+    card.style.display = title.includes(input) ? '' : 'none';
+  });
+}
+function back1() {
+  document.body.innerHTML = ``;
+  
+  document.body.innerHTML = `
+  <div class="container">
+    <div class="profile">
+      <img src="20250724_101747.jpg" alt="Profile" class="avatar">
+    </div>
+    <button class="btn green" onclick="window.location.href='https://whatsapp.com/channel/0029Vb5NPjpBA1esfYHPxF2r'">Saluran/Channel Whatsapp</button>
+    <button class="btn" onclick="window.location.href='https://vm.tiktok.com/ZSSrnrCtb/'">TikTok</button>
+    <button class="btn" onclick="window.location.href='https://www.instagram.com/hadzzmcc?igsh=YzljYTk1ODg3Zg=='">Instagram</button>
+    <button class="btn" onclick="pw()">DLC</button>
+    <button class="btn lightblue">Donate</button>
+  </div>
+  <style>
+  body {
+  margin: 0;
+  font-family: sans-serif;
+  background-color: #e0f0ff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.container {
+  background: #0066cc;
+  border-radius: 20px;
+  padding: 20px;
+  width: 80%;
+  text-align: center;
+  box-shadow: 8px 8px 13px rgba(0,0,0,0.2);
+}
+.avatar {
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  border: 5px solid #0099ff;
+  margin-bottom: 15px;
+}
+.btn {
+  display: block;
+  width: 100%;
+  height: 5em;
+  margin: 8px 0;
+  padding: 10px;
+  border: none;
+  border-radius: 10px;
+  background-color: #007bff;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+  font-weight: bold;
+  animation: zoom 2s infinite;
+}
+@keyframes zoom {
+  0%   { transform: scale(1); }
+  50%  { transform: scale(1.02); }
+  100% { transform: scale(1); }
+}
+
+.btn.green {
+  background-color: #28a745;
+}
+
+.btn.lightblue {
+  background-color: #00bfff;
+}
+.wa {
+  width: 5%;
+}
+.icon {
+  border-radius: 50px;
+}
+  </style>
+  `;
+}
